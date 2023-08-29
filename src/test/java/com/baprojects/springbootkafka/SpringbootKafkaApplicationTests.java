a1f69f65528af6934f623d3ccdbd57e2c33c7ea9
brian.ademba <brian.ademba@cellulant.io>
2023-08-29 22:45:19 +0300

    first commit

---
+++ src/test/java/com/baprojects/springbootkafka/SpringbootKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baprojects.springbootkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
